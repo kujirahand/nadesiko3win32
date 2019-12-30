5d0fc1cbbc084914728a0871cf606a3cd973df67
kujirahand <web@kujirahand.com>
2019-12-30 22:39:40 +0800

    不足しているファイルを追加

---
+++ src/plugin_test.js
@@ -0,0 +1,26 @@
+/**
+ * file: plugin_test.js
+ * テスト実行用プラグイン
+ */
+const PluginTest = {
+  // @テスト
+  'ASSERT等': { // @ mochaによるテストで、ASSERTでAとBが正しいことを報告する // @ASSERTひとしい
+    type: 'func',
+    josi: [['と'], ['が']],
+    fn: function (a, b, sys) {
+      const assert = require('assert')
+      assert.strictEqual(a, b)
+    }
+  },
+  // @テスト
+  'なる': { // @ mochaによるテストで、ASSERTでAとBが正しいことを報告する // @なる
+    type: 'func',
+    josi: [['と'], ['に']],
+    fn: function (a, b, sys) {
+      const assert = require('assert')
+      assert.strictEqual(a, b)
+    }
+  }
+}
+
+module.exports = PluginTest
