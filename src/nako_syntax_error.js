c55678e36759986db95ae4c08e6a6ef3f5f1978c
kujirahand <web@kujirahand.com>
2020-01-16 14:56:58 +0800

    v3.0.67

--- src/nako_syntax_error.js
+++ src/nako_syntax_error.js
@@ -1,11 +1,18 @@
 /**
  * なでしこの文法エラーを表示するクラス
  */
+const PluginSystem = require('./plugin_system')
+
 class NakoSyntaxError extends Error {
   constructor (msg, line, fname) {
     const line2 = line + 1
     const fname2 = (fname === undefined) ? '' : fname
-    const title = `[文法エラー]${fname}(${line2}行目): ${msg}`
+    // Get Nadesiko Version
+    const dummy = {__v0: {}}
+    PluginSystem['!PluginSystem:初期化'].fn(dummy)
+    // Error Message Format
+    const title = `[文法エラー]${fname2}(${line2}行目): ${msg}\n` +
+                  `[バージョン] ${dummy.__v0.ナデシコバージョン}`
     super(title)
   }
 }
