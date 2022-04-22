536d60026d991ac44ad7d3979cd9cc2c61b4249d
kujirahand <web@kujirahand.com>
2022-04-22 15:37:35 +0900

    v3.2.2を反映

---
+++ editor/version_main.jsx
@@ -0,0 +1,12 @@
+// version_main.js
+import React from 'react'
+import ReactDOM from 'react-dom'
+import nakoVersion from '../src/nako_version.mjs'
+
+try {
+  for (const e of Array.from(document.getElementsByClassName('version-component'))) {
+    ReactDOM.render(<div>日本語プログラミング言語「なでしこ3」<br/>Ver. {nakoVersion.version}</div>, e)
+  }
+} catch (e) {
+  console.error(e) // IE11
+}
