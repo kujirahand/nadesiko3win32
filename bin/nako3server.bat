9b9269ad681d3d55a4aad6b3716ead71e011a834
kujirahand.mac2021.win11 <web@kujirahand.com>
2024-07-23 18:44:21 +0900

    changed menu app : execute bin dir's bat

--- bin/nako3server.bat
+++ bin/nako3server.bat
@@ -4,7 +4,7 @@ pushd %~dp0..
 set ROOT_DIR=%CD%
 popd
 
-SET SRC_DIR=%ROOT_DIR%\node_modules\nadesiko3\src
+SET TOOLS_DIR=%ROOT_DIR%\node_modules\nadesiko3\tools
 SET CNAKO_JS=%SRC_DIR%\cnako3.mjs
 if exist %ROOT_DIR%\nodejs\node.exe (
     SET NODE_EXE=%ROOT_DIR%\nodejs\node.exe
@@ -13,6 +13,6 @@ if exist %ROOT_DIR%\nodejs\node.exe (
 )
 
 
-%NODE_EXE% %SRC_DIR%\nako3server.mjs %1 %2 %3 %4 %5 %6 %7 %8 %9
+%NODE_EXE% %TOOLS_DIR%\nako3server\index.mjs %1 %2 %3 %4 %5 %6 %7 %8 %9
 pause
 
